18319b9063547933827dabf7e897956e9ee471d3
Unknown <mauricederidder1@gmail.com>
2019-02-06 21:55:33 +0100

    Add project, start on player
    player: 
    - shooting
    - movement
    - camera follow
    
    scene:
    - basic floor
    - trees for reference

---
+++ 2dplatformgame/Assets/Arrow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private GameObject arrow;
+    private GameObject player;
+    bool arrowCreated = false;
+    bool arrowFlying = false;
+    public float arrowRange;
+    public float arrowSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("MainCharacter");
+        arrow = GameObject.Find("arrow"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FireArrow();
+    }
+    public void FireArrow()
+    {
+        arrow = GameObject.Find("arrow");
+        // TODO:: Rotate arrow, make arrow fly
+        // creating the arrow
+
+        //  arrow.AddComponent<Rigidbody2D>();
+        //  arrow.AddComponent<CapsuleCollider2D>();
+
+
+        //  arrow.gameObject.name = "arrow";
+
+
+        if (arrowFlying == true)
+        {
+            arrow.transform.TransformDirection(new Vector3(arrowSpeed, -1, 10));
+            Debug.Log("te");
+        }
+        
+
+    }
+    public void CreateArrow()
+    {
+
+        player = GameObject.Find("MainCharacter");
+        GameObject arrow = GameObject.Find("Arrow");
+
+        arrow.transform.position = player.transform.position;
+
+        arrow.transform.rotation = new Quaternion(0, 0, 90, 90);
+        arrowCreated = true;
+        arrowFlying = true;
+
+        if (arrowCreated == true && arrowFlying == true)
+        {
+            Update();
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        arrowFlying = false;
+    }
+}
---
+++ 2dplatformgame/Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D player;
+    private Arrow arrow = new Arrow();
+    
+    public float movementSpeed;
+    
+    bool grounded = true;
+    
+    bool firing;
+    public float jumpspeed;
+    Vector3 jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jump = new Vector2(0.0f, 2.0f);
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForInput();
+    }
+    void CheckForInput()
+    {
+        
+        if (Input.GetKey("a"))  
+        {
+            Move("a");
+        }
+        if (Input.GetKey("d"))
+        {
+            Move("d");
+        }
+        if (Input.GetKey("space"))
+        {
+            Move("space");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            arrow.CreateArrow();
+        }
+    }
+    void Move(string direction)
+    {
+        movementSpeed -= player.velocity.x; 
+        if (direction == "a")
+        {
+            player.transform.position -= new Vector3(movementSpeed,0,0);
+        }
+        if (direction == "space" && grounded == true)
+        { 
+            player.AddForce(jump * jumpspeed, ForceMode2D.Impulse);
+            Debug.Log("yo");
+            grounded = false;
+        }
+        if (direction == "d")
+        {
+            player.transform.position += new Vector3(movementSpeed, 0, 0);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        grounded = true;
+    }
+
+    
+    
+}
